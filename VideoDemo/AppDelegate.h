8f15d95fadcaea1aaa23c1d9442a608b6d9a73bc
sumaomao1414 <774538210@qq.com>
2018-05-26 13:18:44 +0800

    Initial Commit

---
+++ VideoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoDemo
+//
+//  Created by maomao on 2018/5/26.
+//  Copyright © 2018年 maomao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
